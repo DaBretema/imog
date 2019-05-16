bug fix on motion calculation

plotter.py
@@ -2,20 +2,21 @@
 
 import sys
 import numpy as np
-
-import matplotlib.cm as cm
+import matplotlib.colors as cl
 import matplotlib.pyplot as plt
 
-data = np.loadtxt(sys.argv[1],unpack=True)
-dMax = abs(data).max()
-dLen = len(data)
+filePrefix = sys.argv[1]
+dataFile = filePrefix+"__heatmap.txt"
+markFile = filePrefix+"__refFrames.txt"
 
-# plt.imshow(data, cmap='hot', interpolation='quadric')
-# plt.colorbar()
-# plt.show()
+data = np.loadtxt(dataFile, unpack=False)
+axisDataMax =  len(data) if (len(data) > len(data[0])) else len(data[0])
+print(axisDataMax)
 
-plt.imshow(data, interpolation='bilinear', cmap=cm.RdYlGn,
-                origin='lower', extent=[0, dLen, 0, dLen],
-                vmax=dMax, vmin=0)
+mark = np.loadtxt(markFile, unpack=True)
+
+
+plt.scatter(mark[0], mark[1], s=50, cmap="hot")
+plt.imshow(data, origin='lower', extent=[0, axisDataMax, 0, axisDataMax])
 plt.colorbar()
 plt.show()